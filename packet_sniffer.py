467c2e4a2e3aadcd2bb6733f34447acd273fb7b2
Aidan Scammell <131626872+GreekYogurtt@users.noreply.github.com>
2026-09-23 18:19:29 -0500

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,7 +4,8 @@ from scapy.all import sniff, wrpcap
 
 
 class PacketSniffer:
-
+# Set up the network and file where packets will be saved
+    
     def __init__(self, interface="wlan0",
                  output_file="capture.pcap"):
         self.interface = interface
@@ -13,12 +14,12 @@ class PacketSniffer:
 
     def start(self):
         print("Starting packet capture...")
-
+# Start sniffing packets/traffic only on ports 80 and 9000
         self.packets = sniff(
             iface=self.interface,
             filter="tcp port 80 or tcp port 9000"
         )
-
+# Save the captured packets to a PCAP file
     def save(self):
         wrpcap(self.output_file, self.packets)
 
